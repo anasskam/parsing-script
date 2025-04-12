cdffa2972e3f9ecc6b0ce4f25c8ec38a443f4ef4
anasskam <anasskam361@gmail.com>
2025-04-12 17:56:08 +0100

    fix some errors

--- parser.cpp
+++ parser.cpp
@@ -6,28 +6,22 @@
 class Parser {
 
 public:
-    Parser(const std::string& filePath);
+    Parser(const std::string& filePath): m_FilePath(filePath) {
+        init();
+    }
 
 private:
-    void init();
+    void init() {
+        try {
+            std::string cmd = "pdftotext -f 1 -l 1 ../pdfs/" + m_FilePath + " temp.out";
+            
+            int status = system(cmd.c_str());
+
+            if(status == 1) throw std::runtime_error("Text extraction failed.");
+
+        } catch(const std::runtime_error& error) {
+            std::cerr << error.what() << '\n';
+        }
+    }
     std::string m_FilePath;
 };
-
-Parser::Parser(const std::string& filePath): m_FilePath(filePath){
-    init();
-}
-
-void Parser::init() {
-
-    try {
-        std::string cmd = "pdftotext -f 1 -l 1 \"../pdfs/" + m_FilePath + " temp.out";
-        
-        int status = system(cmd.c_str());
-
-        if(status == 1) throw std::runtime_error("Text extraction failed.");
-
-    } catch(const std::runtime_error& error) {
-        std::cerr << error.what() << '\n';
-    }
-    
-}
